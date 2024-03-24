1ccb573917bcb0a8e466db88a6e5d9f1b6da3677
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2024-03-24 19:53:39 +0330

    Pipe Spawner
    Spawning Pipes...

---
+++ PipeSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heghtRange = 0.45f;
+    [SerializeField] private GameObject _pipe;
+
+    private float _timer;
+
+    private void Start()
+    {
+        SpawnPipe();
+    }
+
+    private void Update()
+    {
+        if (_timer > _maxTime)
+        {
+            SpawnPipe();
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
+    }
+
+    private void SpawnPipe()
+    {
+        Vector3 spawnPose = transform.position + new Vector3(0, Random.Range(_heghtRange, _heghtRange));
+        GameObject pipe = Instantiate(_pipe, spawnPose, Quaternion.identity);
+        Destroy(_pipe, 10f);
+    }
+}
